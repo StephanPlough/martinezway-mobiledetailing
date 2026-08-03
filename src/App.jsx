999617d126bc58d2b70e063a0e54f06bae4f5f54
Stephan Plough <sgploug3@wgu.edu>
2026-08-02 20:41:46 -0700

    Changed BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import { useState } from "react";
-import "./App.css";
-import "./components/Navbar/Navbar.css";
-import "./components/Hero/Hero.css";
-import "./components/Footer/Footer.css";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navbar/Navigation.jsx";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 
+import "./App.css";
+import "./components/Navbar/Navbar.css";
+import "./components/Hero/Hero.css";
+import "./components/Footer/Footer.css";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Navigation />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <Footer />
     </div>
   );
