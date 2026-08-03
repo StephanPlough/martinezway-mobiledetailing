d9b2093ddb82d390181c326ca381685222dbd31f
Stephan Plough <sgploug3@wgu.edu>
2026-08-02 21:25:01 -0700

    Made components responsive to mobile/tablets

--- src/components/Navbar/Navigation.jsx
+++ src/components/Navbar/Navigation.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Hero from "../Hero/Hero";
-import BookingButton from "../Hero/BookingButton";
 
 export default function Navigation() {
   return (
@@ -10,7 +8,8 @@ export default function Navigation() {
         src="/martinezway-mobiledetailing/img/log.png"
         className="nav-logo-img"
         alt="Martinez Way Logo"
-      ></img>
+      />
+
       <ul className="nav-links">
         <li>
           <Link to="/">Home</Link>
